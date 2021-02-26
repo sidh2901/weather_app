26e19778a5ccca823caac228017524d9250eed6d
sidh2901 <59162267+sidh2901@users.noreply.github.com>
2021-02-26 00:55:09 -0800

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React,{useState,useRef,useEffect} from 'react';
 import './App.css';
+import axios from "axios"
+import moment from 'moment';
+
 
 function App() {
+
+  const [weatherInfo, setWeatherInfo] = useState(null);
+  const inputRef = useRef(null);
+  const [image, setImage] = useState(null)
+
+  useEffect(() => {
+    fetchWeatherInfo();
+  }, [])
+
+  const fetchWeatherInfo = (e) => {
+     e?.preventDefault();
+    const options = {
+      method: 'GET',
+      url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+      params: {
+        q: inputRef.current.value || "London,uk",
+        units: "metric",
+      },
+      headers: {
+        'x-rapidapi-key': '1a72d0f313msh3ac812888957d57p155018jsn365c7603d4d9',
+        'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com'
+      }
+    };
+        axios
+        .request(options)
+        .then((response) => {
+          console.log(response.data)
+        setWeatherInfo(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+     determineBackgroundImage();
+  }, [weatherInfo])
+
+  const determineBackgroundImage = () => {
+    if (weatherInfo?.main.temp < 10){
+      setImage ("https://media1.popsugar-assets.com/files/thumbor/n8bUt1yPUD0zvO0XmWNFuVj763k/685x0:3345x2660/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2020/01/07/726/n/1922441/419ff9b05e14b139162ef6.64727550_/i/Best-Winter-Photos.jpg")
+    }
+    if (weatherInfo?.main.temp >= 10 && weatherInfo?.main.temp < 25){
+         setImage('https://www.oakbaynews.com/wp-content/uploads/2019/07/17533097_web1_cloud_1.jpg')
+    }
+    
+    if (weatherInfo?.main.temp >= 25){
+      setImage('https://i.pinimg.com/originals/e1/3a/4d/e13a4d737425141353603f7a3edb73cd.jpg')
+ }
+
+  
+  }
+
+
+  console.log(weatherInfo)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="app" style={{backgroundImage: `url(${image})`}}>
+      <div className="app__container">
+        <div className="app__info app__left">
+          <h1>Weather app</h1>
+          <form>
+            <input ref={inputRef} type='text' placeholder="Type the city" />
+            <button onClick={fetchWeatherInfo} type='submit'>Show Me the Weather</button>
+          </form>
+        </div>
+        <div className="app__info app__right">
+          <h2>{weatherInfo?.name}</h2>
+          <h2>{weatherInfo?.main.temp} Degree Celsius</h2>
+          <h3>
+            {weatherInfo && `Sunrise: ${moment.unix(weatherInfo?.sys?.sunrise).format("LLLL")}`}
+            </h3>
+            <h4>{weatherInfo && `Sunset: ${moment.unix(weatherInfo?.sys?.sunset).format("LLLL")}`}</h4>
+        </div>
+        
+      </div>
+      
     </div>
   );
 }
